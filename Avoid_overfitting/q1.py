531a9ed1946aec1caca0b3eec9f5ecb6d39a0078
jihyeon602 <000602jh@naver.com>
2025-05-15 15:11:03 +0900

    .

--- Avoid_overfitting/q1.py
+++ Avoid_overfitting/q1.py
@@ -20,7 +20,10 @@ def build_mlp_model(img_shape):
     model.add(Input(shape=img_shape))
     
     # TODO: [지시사항 1번] 기본 MLP 모델을 만드세요.
+    model.add(layers.Flatten())
     model.add(layers.Dense(1024, activation='relu'))
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dense(10, activation='softmax'))
 
     return model
 
@@ -30,13 +33,20 @@ def build_bn_mlp_model(img_shape):
     model.add(Input(shape=img_shape))
     
     # TODO: [지시사항 2번] Batch Normalizatino이 추가된 MLP 모델을 만드세요.
-    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(1024))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Activation("relu"))
+    model.add(layers.Dense(256))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Activation("relu"))
+    model.add(layers.Dense(10, activation='softmax'))
 
     return model
 
 # 각 모델의 hyperparameter를 설정하고 학습합니다.
 def run_model(model, X_train, X_test, y_train, y_test, epochs=5):
-    model.compile(optimizer="sgd", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
     hist = model.fit(X_train, y_train, epochs=epochs, batch_size=64, shuffle=True, verbose=2)
 
     test_loss, test_acc = model.evaluate(X_test, y_test, verbose=0)
